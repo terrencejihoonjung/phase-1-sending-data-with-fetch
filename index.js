eb44e97013abdf7dab6d8c1ff7c72607ddd2afea
--replace-all <terrencejung928@gmail.com>
2022-05-26 15:52:10 -0700

    commit tree 790538db7bf1bb604bcf911cace7628ea39aaf9f

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            const header = document.createElement("h1");
+            header.textContent = data.id;
+            document.querySelector("body").append(header);
+        })
+        .catch(error => {
+            const errorMessage = document.createElement("p");
+            errorMessage.textContent = error.messsage;
+            document.querySelector("body").append(error);
+        })
+
+}
